dd4c16baef9f8efba83bb8fa0a3137f9562b73ad
ralph.collett <ralph.collett@gmail.com>
2018-08-27 16:49:59 +0100

    Initial import.

---
+++ build.sbt
@@ -0,0 +1,9 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "cafekittens"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.12.6"
+
+scalaJSUseMainModuleInitializer := true
---
+++ src/main/scala/com/cafekittens/CafeKittens.scala
@@ -0,0 +1,16 @@
+package com.cafekittens
+
+object CafeKittens {
+
+  def main(args: Array[String]): Unit = {
+    println(
+      """
+        |                   _..
+        |  /}_{\           /.-'
+        | ( a a )-.___...-'/
+        | ==._.==         ;
+        |      \ i _..._ /,
+        |      {_;/   {_//
+      """.stripMargin)
+  }
+}
