2559f4368519c7ffb5ee45200b4e7d353825fd89
ralph.collett <ralph.collett@gmail.com>
2018-08-28 17:19:13 +0100

    Convert to React.

--- build.sbt
+++ build.sbt
@@ -9,10 +9,13 @@ scalaVersion := "2.12.6"
 scalaJSUseMainModuleInitializer := true
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.6"
-libraryDependencies += "org.querki" %%% "jquery-facade" % "1.2"
 
-skip in packageJSDependencies := false
-jsDependencies +=
-  "org.webjars" % "jquery" % "2.2.1" / "jquery.js" minified "jquery.min.js"
+libraryDependencies ++= Seq(
+  "com.github.japgolly.scalajs-react" %%% "core" % "0.11.3"
+)
 
-jsEnv := new org.scalajs.jsenv.jsdomnodejs.JSDOMNodeJSEnv()
+jsDependencies ++= Seq(
+  "org.webjars.bower" % "react" % "15.3.2" / "react-with-addons.js" minified "react-with-addons.min.js" commonJSName "React",
+  "org.webjars.bower" % "react" % "15.3.2" / "react-dom.js" minified "react-dom.min.js" dependsOn "react-with-addons.js" commonJSName "ReactDOM",
+  "org.webjars.bower" % "react" % "15.3.2" / "react-dom-server.js" minified  "react-dom-server.min.js" dependsOn "react-dom.js" commonJSName "ReactDOMServer"
+)
--- src/main/scala/com/cafekittens/CafeKittens.scala
+++ src/main/scala/com/cafekittens/CafeKittens.scala
@@ -1,18 +1,50 @@
 package com.cafekittens
 
-import org.querki.jquery._
+import japgolly.scalajs.react.vdom.prefix_<^._
+import japgolly.scalajs.react.{BackendScope, Callback, ReactComponentB, ReactDOM, ReactEventI}
+import org.scalajs.dom
+
+import scala.util.Random
 
 object CafeKittens {
 
-  def main(args: Array[String]): Unit = {
-    $("""<button type="button">Click me!</button>""")
-      .click(() => addClickedMessage())
-      .appendTo($("body"))
+  private val component = ReactComponentB[Unit]("Caffeine listings")
+    .initialState(BeverageState(Beverage("", 0)))
+    .renderBackend[BeverageOps]
+    .build
 
-    $("#click-me-button").click(() => addClickedMessage())
+  case class Beverage(name: String, mgOfCaffeine: Double)
+
+  case class BeverageState(beverage: Beverage)
+
+  private val beverages = Seq(
+    Beverage("Tea", 2.2),
+    Beverage("Coffee", 10),
+    Beverage("Red Bull", 7),
+    Beverage("Coca Cola", 5)
+  )
+
+  class BeverageOps($: BackendScope[Unit, BeverageState]) {
+
+    private val beverageState = $.zoom(_.beverage)((s, x) => s.copy(beverage = x))
+
+    def searchForBeverage(event: ReactEventI): Callback = {
+      val randomBeverage = Random.shuffle(beverages).head
+      beverageState.setState(randomBeverage)
+    }
+
+    def render(state: BeverageState) = {
+      <.div(
+        <.h1("Caffeine listings"),
+        <.p(s"${state.beverage.name} ${state.beverage.mgOfCaffeine}"),
+        <.button(^.`type` := "button",
+          ^.onClick ==> searchForBeverage,
+          "Search"
+        ))
+    }
   }
 
-  def addClickedMessage(): Unit = {
-    $("body").append("<p>You clicked the button!</p>")
+  def main(args: Array[String]): Unit = {
+    ReactDOM.render(component(), dom.document.getElementById("playground"))
   }
 }
